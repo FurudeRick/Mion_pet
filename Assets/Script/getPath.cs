63cf6a609295f01453e43e8f34bdc8b211769cb0
לבדה ללכת תקוה <3573976859@qq.com>
2022-06-26 15:30:52 +0800

    0.2

---
+++ Assets/Script/getPath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script
+{
+    class getPath
+    {
+        public static string fileRoute(string file)
+        {
+            string route = file;
+            for (int i = file.Length - 1; route[i] != '\\'; i--)
+            {
+                route = file.Remove(i);
+            }
+            return route;
+        }
+
+        public static string scriptPath()
+        {
+            return System.Windows.Forms.Application.StartupPath + @"\PyRPA-main\PyRPA-main\Source";
+        }
+
+        //获取该目录所有文件夹
+        public static List<string> scriptList(string sourcePath)
+        {
+            List<string> folder = new List<string>();
+            DirectoryInfo theFolder = new DirectoryInfo(sourcePath);
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+            foreach (DirectoryInfo NextFolder in dirInfo)
+            {
+                folder.Add(NextFolder.Name);
+            }
+            return folder;
+        }
+    }
+}
---
+++ Assets/Script/startHook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Assets.Script
+{
+    public class startHook : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            KeyboardHook.HookStart();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
